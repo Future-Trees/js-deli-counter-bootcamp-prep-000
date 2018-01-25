dde581a07368c3233ba04352ee8d855aa8a385f0
Skylar Dunn-Lubin <skylar.dunnlubin@gmail.com>
2018-01-25 22:07:10 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function currentLine(line){
+  if (!line.length){
+    return "The line is currently empty."
+  }
+
+const numberAndNames = []
+
+for (let i=0, l=line.length; i < l; i++){
+  numbersAndNames.push(`${i+1}.${line[i]`)
+}
+}
